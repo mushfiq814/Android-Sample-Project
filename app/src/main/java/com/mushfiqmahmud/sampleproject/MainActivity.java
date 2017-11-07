3f3cfa4ea894466ded8eb542507eab4f8b5b0557
Mushfiq Mahmud <mushfiq@mail.usf.edu>
2017-11-06 21:38:41 -0500

    Nov 6

--- app/src/main/java/com/mushfiqmahmud/sampleproject/MainActivity.java
+++ app/src/main/java/com/mushfiqmahmud/sampleproject/MainActivity.java
@@ -2,12 +2,40 @@ package com.mushfiqmahmud.sampleproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnClick;
+    Button btnReset;
+    TextView textCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnClick = (Button)findViewById(R.id.btnClick);
+        btnReset = (Button)findViewById(R.id.btnReset);
+        textCounter = (TextView)findViewById(R.id.textViewCounter);
+
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String countValue = textCounter.getText().toString();
+                int count = Integer.parseInt(countValue);
+                count++;
+                textCounter.setText(String.valueOf(count));
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textCounter.setText(String.valueOf(0));
+            }
+        });
     }
 }
