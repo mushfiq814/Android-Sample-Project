42a6e130cdcb783b1c27dad834becd5fec5d8acf
Mushfiq Mahmud <mushfiq@mail.usf.edu>
2017-11-07 12:20:57 -0500

    Experimented with Second Page

--- app/src/main/java/com/mushfiqmahmud/sampleproject/MainActivity.java
+++ app/src/main/java/com/mushfiqmahmud/sampleproject/MainActivity.java
@@ -1,24 +1,29 @@
 package com.mushfiqmahmud.sampleproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btnClick;
+    Button btnClick; // Create button and textview objects to be used later
     Button btnReset;
+    Button btnNewScreen;
     TextView textCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // This connects the main activity view xml file to this java file
 
         btnClick = (Button)findViewById(R.id.btnClick);
         btnReset = (Button)findViewById(R.id.btnReset);
+        btnNewScreen = (Button)findViewById(R.id.btnNewScreen);
         textCounter = (TextView)findViewById(R.id.textViewCounter);
 
         btnClick.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +40,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 textCounter.setText(String.valueOf(0));
+            }
+        });
+
+        btnNewScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/mushfiqmahmud/sampleproject/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.mushfiqmahmud.sampleproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        TextView textView = (TextView)findViewById(R.id.textView);
+    }
+}
